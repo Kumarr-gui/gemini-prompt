358fd4e5ebb2ac209bede72fc43387989df9e35d
Ankit <ankitsinghsmps@gmail.com>
2025-02-06 17:03:05 +0530

    added api for postman

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import express from 'express';
 import 'dotenv/config'
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-const prompt = "Who is Virat Kohli";
+const prompt = "what is JSON format";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
+
+const app = express();
+app.use(express.json());
+
+app.post('/generate', async (req , res)=>{
+    try{
+        const { prompt}  = req.body;
+        if(!prompt){
+            return res.status(400).json({error: "Prompt is required"})
+        }
+        const result = await model.generateContent(prompt);
+        const responseText = result.response.text();
+
+        res.json({ response: responseText })
+    }catch(error){
+        console.error("Error generating response:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+)
+app.listen(process.env.PORT, ()=>{
+    console.log("Port is listening at 5505")
+})
